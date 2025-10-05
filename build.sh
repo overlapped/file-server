15db179c909dc29d050ad1fc0f09efa6c3c44a20
aserebryakov <overlapped@gmail.com>
2025-10-05 22:24:17 +0700

    Initial commit

--- build.sh
+++ build.sh
@@ -14,6 +14,14 @@ cd ..
 # Запуск клиента (в другом терминале)
 # ./file_client 127.0.0.1 8080 my_download.bin
 
+# Запуск нескольких клиентов
+# for i in {1..3}; do
+#    echo Run client $i...
+#    ./file_client 127.0.0.1 4000 "download_$i.bin" &
+# done
+
+# wait
+
 # Запуск тестов
 # ./file_server_tests
 # ./file_server_benchmarks
